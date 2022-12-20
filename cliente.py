a45c0f9ee14d4299846e1ef12d1e126847f86d85
Juliana Ferreira <julianafercavalcante@hotmail.com>
2022-12-20 03:20:04 -0300

    implementação de threads

--- cliente.py
+++ cliente.py
@@ -1,100 +1,122 @@
 import socket
+import threading
+from threading import Thread, Semaphore
+from style import style
+#from main import protocolo
 
 HOST = 'localhost'
-PORT = 1500
-udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+PORT = 5100
+cliente = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 dest = (HOST, PORT)
 
-print("\n--- Bem-vindo ao Say Hello ---\n")
+print("\n--- Bem-vindo ao Say Hello! ---\n")
 
-username = input('Username: ')
-udp.sendto(username.encode(), dest)
-codigo, servidor = udp.recvfrom(2048)
-print(codigo.decode())
-while (len(username) < 1) and (codigo.decode() == 'Este nome já está sendo utilizado'):
-    print('Digite um username\n')
-    username = input('Username: ')
-    udp.sendto(username.encode(), dest)
-    codigo, servidor = udp.recvfrom(2048)
-    print(codigo.decode())
+def connect():
+    try:
+        cliente.connect(dest)
+        print(f'{style.GREEN}• Conectado\n{style.RESET}')
+    except:
+        print(f'{style.RED}• Não foi possiível conectar-se{style.RESET}\n')
+    username = 'username '
+    username += input('username: ')
+    while len(username) == 'username ':
+        print(f'{style.RED}Digite um username{style.RESET}\n')
+        username += input('username: ')
+    cliente.sendall(username.encode())
 
-blockMutex = 0
+    # msg = cliente.recv(2048).decode()
+    # print(msg)
 
-while True:
+    # threading.Thread(target=msg_servidor, args=(cliente, )).start()
+     
+
+def entrada():
+    while True:
+        entrada = input('\nSH >>> ')
+        cliente.sendall(entrada.encode())
+        codigo, servidor = cliente.recvfrom(2048)
+        print(codigo.decode())
+
+
+def msg_servidor(cliente):
+    while True:
+        msg = cliente.recv(2048).decode()
+        if msg != '':
+            username = msg.split("->")[0]
+            print(msg.split('->'))
+            content = msg.split("->")[1]
+        print(f"[{username}] {content}")
+
+connect()
+entrada()
+
+# Receber = threading.Thread(target=, args=())
+# Enviar = threading.Thread(target=, args=())
+# while True:
     
-    entrada = input("\nSH >>> ")
-    entrada = entrada.split()
-    comando = entrada[0].upper()
-  
-    if comando == "CREATE":
-        enter_user = f'{entrada}+{username}+{blockMutex}'
-        udp.sendto(enter_user.encode(), dest) # mandou a entrada
-        quantInput, servidor = udp.recvfrom(2048) # recebeu a mend=sagem de input
-        quantInput = input(quantInput.decode()) # fez o input
-        udp.sendto(quantInput.encode(), dest) # mandou para o servidor
-        codigo, servidor = udp.recvfrom(2048) #recebeu o resultado
-        print(codigo.decode())
+#     entrada = input("\nSH >>> ")
+#     entrada_split = entrada.split()
+#     comando = entrada_split[0].upper()
+#     parametro = ''
+#     for i in entrada_split[1:]:
+#         parametro += f'{i} '
+#     enter_user = f'{entrada}+{username}'
+#     cliente.sendto(enter_user.encode(), dest)
+
+#     # protocolo(username, cliente)
+
+#     if comando == "CREATE":
+#         codigo, servidor = cliente.recvfrom(2048)
+#         print(codigo.decode())
       
-    elif comando == "DELETE":
-        enter_user = f'{entrada}+{username}+{blockMutex}'
-        udp.sendto(enter_user.encode(), dest)
-        codigo, servidor = udp.recvfrom(2048)
-        print(codigo.decode())
+#     elif comando == "DELETE":
+#         codigo, servidor = cliente.recvfrom(2048)
+#         print(codigo.decode())
       
-    elif comando == "JOIN":
-        blockMutex = 1
-        enter_user = f'{entrada}+{username}+{blockMutex}'
-        udp.sendto(enter_user.encode(), dest)
-        codigo, servidor = udp.recvfrom(2048)
-        if codigo.decode() == '200 +OK':
-            while True:
-                msg = input()
-                msg = msg.encode
-                udp.send(msg)
+#     elif comando == "JOIN":
+#         codigo, servidor = cliente.recvfrom(2048)
+#         if codigo.decode() == '200 +OK':
+#             while True:
+#                 msg = input()
+#                 msg = msg.encode
+#                 cliente.send(msg)
       
-    elif comando == "LEAVE":
-        if len(entrada) != 1:
-            print('\n400 -ERR este método não recebe parâmetros!')
-            continue
-        if blockMutex == 1:
-            id_sala = None
-            blockMutex = 0
-            print('\n200 +OK')
-        else:
-            print('\n400 -ERR você não está em nenhum chat!')
+#     elif comando == "LEAVE":
+#         if len(entrada) != 1:
+#             print('\n400 -ERR este método não recebe parâmetros!')
+#             continue
+#             id_sala = None
+#             print('\n200 +OK')
 
-    elif comando == 'CHATS':
-        out, servidor = udp.recvfrom(2048)
-        out = out.decode().split('!')
-        if out[0] == '\n400 -ERR este método não recebe parâmetros!' or out[0] == '\n400 -ERR servidor ainda não possui chats!':
-            print(f'\n{out[0]}')
-        else:
-            print(f'\n{out[0]}')
-            print(out[1])
+#     elif comando == 'CHATS':
+#         out, servidor = cliente.recvfrom(2048)
+#         out = out.decode().split('!')
+#         if out[0] == '\n400 -ERR este método não recebe parâmetros!' or out[0] == '\n400 -ERR servidor ainda não possui chats!':
+#             print(f'\n{out[0]}')
+#         else:
+#             print(f'\n{out[0]}')
+#             print(out[1])
 
-    # elif comando == "MEMBERS":
-    #     if blockMutex == 0:
-    #         codigo, servidor = udp.recvfrom(2048)
-    #         print(codigo.decode())
+#     # elif comando == "MEMBERS":
+#     #     if blockMutex == 0:
+#     #         codigo, servidor = cliente.recvfrom(2048)
+#     #         print(codigo.decode())
 
-    #     else:
-    #         print('\n400 -ERR você deve estar em um chat para usar este parâmetro!')
+#     #     else:
+#     #         print('\n400 -ERR você deve estar em um chat para usar este parâmetro!')
       
-    elif comando == "QUIT":
-        if blockMutex == 0:
-            break
-        else:
-            print('\,400 -ERR você só pode usar esse comando quando sair do chat atual!')
+#     elif comando == "QUIT":
+#         break
 
-    else:
-        print(f'''\nDigite um comando válido.
-{'='*30}
-CREATE → criar um chat
-DELETE → deletar um chat
-JOIN → entrar em um chat
-LEAVE → sair de um chat
-CHATS → imprime a árvore de chats
-MEMBERS → imprime a lista de pessoas em cada chat
-QUIT → encerra a conexão''')
+#     else:
+#         print(f'''\nDigite um comando válido.
+# {'='*30}
+# CREATE → criar um chat
+# DELETE → deletar um chat
+# JOIN → entrar em um chat
+# LEAVE → sair de um chat
+# CHATS → imprime a árvore de chats
+# MEMBERS → imprime a lista de pessoas em cada chat
+# QUIT → encerra a conexão''')
 
-print("\n--- Obrigado por ter usado Say Hello! ---\n")
+# print("\n--- Obrigado por ter usado Say Hello! ---\n")
--- servidor.py
+++ servidor.py
@@ -1,128 +1,119 @@
-import threading
 import socket
+import threading
+from threading import Thread, Semaphore
+import time
 from arvore import ABB
 from lista import Lista
-
-arvore = ABB()
-lista = Lista()
-lista.insere_no_inicio('Pessoas online\n')
-# sala = threading.Semaphore(1)
+from style import style
 
 HOST = 'localhost'
 PORT = 1500
+limite = 50
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 orig = (HOST, PORT)
 udp.bind(orig)
 
-class style():
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    BLUE = '\033[34m'
-    RESET = '\033[0m'
+arvore = ABB()
+lista = Lista()
+lista.insere_no_inicio('Pessoas online\n')
+semaforoA = Semaphore(1)
+semaforoL = Semaphore(1)
 
-print(f"{' '*10}Acorde! \n-- Say Hello está no ar! --\n\n")
+print(f"{' '*2}Sorria e Acene! \n--- Say Hello! ---\n\n")
 
-blockMutex = 0
-
-while True:
-
-    usuario, cliente = udp.recvfrom(2048)
-    usuario = usuario.decode()
-    if lista.buscar(usuario):
-        codigo = 'Este nome já está sendo utilizado'
-    else:
-        lista.inserir(lista.cabeca, usuario)
-        codigo = f'{style.BLUE}• Conectando no Protocolo{style.RESET}'
-    udp.sendto(codigo.encode(), cliente)
-
-
-    enter_user, cliente = udp.recvfrom(2048)
-    enter_user = enter_user.decode().split('+')
-    entrada = enter_user[0].split()
-    comando = entrada[0].upper()
-    usuario = enter_user[1]
-    blockMutex = enter_user[2]
-
-
-    if comando == 'CREATE':
-        if blockMutex == 0:
-            quantInput = 'Quantidade máxima de pessoas no chat: '
-            udp.sendto(quantInput.encode(), cliente)
-            quantInput, cliente = udp.recvfrom(2048)
-            try:
-                quantInput = int(quantInput.decode())
-            except:
-                codigo = f'{style.RED}400 -ERR quantidade deve ser um número inteiro{style.RESET}'
-                udp.sendto(codigo.encode(), cliente)
-                continue
+def protocolo(address, cliente, entrada):
+    while True:
+        # entrada, address = udp.recvfrom(2048)
+        # entrada = str(entrada)
+        # entrada = entrada[2:-1]
+        # # entrada, cliente = udp.recvfrom(2048)
+        # # entrada = entrada.decode()
+        # try:
+        #     entrada = entrada.split()
+        # except:
+        #     pass
+        # comando = entrada[0].upper()
+        entrada = str(entrada)
+        entrada = entrada.split()
+        comando = entrada[0].upper()
+    
+        if comando == 'CREATE':
+            entrada = entrada[1:]
             nome = ''
-            for i in entrada[1:-1]:
+            for i in entrada:
                 nome += f'{i} '
             if arvore.busca(hash(nome)):
                 codigo = f'{style.RED}400 -ERR chat já existente!{style.RESET}'
             else:
+                semaforoA.acquire()
                 arvore.inserir(nome)
-                print(f'{style.BLUE}{usuario} criou o chat: {nome}\n{style.RESET}')
+                semaforoA.release()
+                time.sleep(0.5)
+                print(f'{style.BLUE}{cliente} criou o chat: {nome}\n{style.RESET}')
                 codigo = f'{style.GREEN}200 +OK sala criada com sucesso!{style.RESET}'
-        else:
-            codigo = f'{style.RED}\n400 -ERR você só pode usar esse comando quando sair do chat atual!{style.RESET}'
-        udp.sendto(codigo.encode(), cliente)
-
-
-    elif comando == 'DELETE':
-        if len(entrada) == 1:
-            codigo = f'{style.RED}400 -ERR este método recebe parâmetros!{style.RESET}'
-        else:
-            nome = ''
-            for i in entrada[1:]:
-                nome += f'{i} '
-            if blockMutex == 0:
+            udp.sendto(codigo.encode(), address)
+            # print('B')
+    
+    
+        elif comando == 'DELETE':
+            if len(entrada) == 1:
+                codigo = f'{style.RED}400 -ERR este método recebe parâmetros!{style.RESET}'
+            else:
+                nome = ''
+                for i in entrada[1:]:
+                    nome += f'{i} '
                 if arvore.busca(hash(nome)):
+                    semaforoA.acquire()
                     arvore.remover(hash(nome))
-                    print(f'{style.BLUE}{usuario} removeu o chat: {nome}\n{style.RESET}')
+                    semaforoA.release()
+                    print(f'{style.BLUE}{cliente} removeu o chat: {nome}\n{style.RESET}')
+                    time.sleep(1)
                     codigo = f'{style.GREEN}200 +OK sala removida com sucesso!{style.RESET}'
                 else:
                     codigo = f'{style.RED}400 -ERR este chat não existe!{style.RESET}'
+            udp.sendto(codigo.encode(), cliente)
+    
+    
+        elif comando == "JOIN":
+            if len(entrada) == 1:
+                codigo = f'{style.RED}400 -ERR este método recebe parâmetros!{style.RESET}'
             else:
-                codigo = f'{style.RED}\n400 -ERR você só pode usar esse comando quando sair do chat!{style.RESET}'
-        udp.sendto(codigo.encode(), cliente)
+                nome = ''
+                for i in entrada[1:]:
+                    nome += f'{i} '
+                if arvore.busca(hash(nome)):
+                    semaforoL.acquire()
+                    lista.remover(cliente)
+                    lista.inserir(lista.cabeca, cliente)
+                    semaforoL.release()
+                    time.sleep(1)
+                    codigo = f'{style.GREEN}200 +OK{style.RESET}'
+                else:
+                    codigo = f'{style.RED}400 -ERR esta sala não existe!{style.RESET}'
+            udp.sendto(codigo.encode(), cliente)
+            msg = udp.recv(4096)
+            msg = f'{cliente}: {msg.decode()}'
+            cursor = lista.cabeca
+            while cursor:
+                if cursor.chat == hash(nome):
+                    udp.send(msg.encode())
+                cursor = cursor.prox
+    
+    
+        elif comando == 'CHATS':
+            if len(entrada) != 1:
+                codigo = f'{style.RED}\n400 -ERR este método não recebe parâmetros!{style.RESET}'
+            elif arvore.raiz == None:
+                codigo = f'{style.RED}\n400 -ERR servidor ainda não possui chats!{style.RESET}'
+            codigo = f'{style.GREEN}200 +OK\n{style.RESET}'
+            preordem = ''
+            for i in (arvore.preordem()):
+                preordem += f'• {i}\n'
+            out = f'{codigo}!{preordem}'
+            udp.sendto(out.encode(), cliente)
 
-
-    elif comando == "JOIN":
-        if len(entrada) == 1:
-            codigo = f'{style.RED}400 -ERR este método recebe parâmetros!{style.RESET}'
-        else:
-            nome = ''
-            for i in entrada[1:]:
-                nome += f'{i} '
-            if arvore.busca(hash(nome)):
-                lista.remover(usuario)
-                lista.inserir(lista.cabeca, usuario, hash(nome))
-                codigo = f'{style.GREEN}200 +OK{style.RESET}'
-            else:
-                codigo = f'{style.RED}400 -ERR esta sala não existe!{style.RESET}'
-        udp.sendto(codigo.encode(), cliente)
-        msg = udp.recv(4096)
-        msg = f'{usuario}: {msg.decode()}'
-        cursor = lista.cabeca
-        while cursor:
-            if cursor.chat == hash(nome):
-                udp.send(msg.encode())
-            cursor = cursor.prox
-
-    elif comando == 'CHATS':
-        if len(entrada) != 1:
-            codigo = f'{style.RED}\n400 -ERR este método não recebe parâmetros!{style.RESET}'
-        elif arvore.raiz == None:
-            codigo = f'{style.RED}\n400 -ERR servidor ainda não possui chats!{style.RESET}'
-        codigo = f'{style.GREEN}200 +OK\n{style.RESET}'
-        preordem = ''
-        for i in (arvore.preordem()):
-            preordem += f'• {i}\n'
-        out = f'{codigo}!{preordem}'
-        udp.sendto(out.encode(), cliente)
-
-
+        # print('b')
+    
     # elif comando == 'MEMBERS':
     #     if blockMutex == 0:
     #         if len(entrada) == 1:
@@ -135,3 +126,39 @@ while True:
     #     else:
     #         codigo = '\n400 -ERR você só pode usar esse comando quando sair do chat!'
     #     udp.sendto(codigo.encode(), cliente)
+
+def mensagem_unicast(cliente, msg):
+    udp.sendto(msg.encode(), cliente)
+
+def mensagem_broadcast(msg):
+    for user in lista:
+        mensagem_unicast(user, msg)
+
+def adm_cliente(address, cliente):
+    lista.inserir(lista.cabeca, address)
+    cliente = str(cliente)
+    cliente = cliente[2:-1]
+    clienteN = cliente.split()
+    if clienteN[0] == 'username':
+        username = clienteN[1]
+        print(f'{style.BLUE}Cliente {username} conectado{style.RESET}\n')
+    # threading.Thread(target=protocolo, args=(address, username, cliente, )).start()
+    print('a')
+    protocolo(address, username, cliente)
+
+
+def main():
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        servidor.bind((HOST, PORT))
+    except:
+        pass
+    while True:
+        cliente, address = udp.recvfrom(2048)
+        threading.Thread(target=adm_cliente, args=(address, cliente, )).start()
+        # thread = threading.Thread(target=adm_cliente, args=(cliente, ))
+        # thread.start()
+
+
+if __name__ == '__main__':
+    main()
